ecac03437f2c724015bc7b9861c5ce5573b8e317
Vlad Petrov <hiffchanel@gmail.com>
2022-11-13 23:58:07 +0300

    load landing page

---
+++ index.js
@@ -0,0 +1,9 @@
+function imgSlider(image) {
+    const starbucks = document.querySelector('.starbucks');
+    starbucks.src = image;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.backgroundColor = color;
+}
